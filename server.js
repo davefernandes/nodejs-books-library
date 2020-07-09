45e8be449f99a905cd78bd61b15fc3a23e1ae5dc
Dave Fernandes <fernandes.dave@gmail.com>
2020-07-09 16:51:59 +0100

    Added error handler for routes not found

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var createError = require('http-errors');
 var express     = require('express');
 var path        = require('path');
 var bodyParser  = require('body-parser');
@@ -45,6 +46,22 @@ app.use('/author', authorRouter);
 app.use('/category', categoryRouter);
 app.use('/copy', copyRouter);
 app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+  
+  // error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 /**
 * Get port from environment and store in Express.
